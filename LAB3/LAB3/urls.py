fc3105db211dba79a20fcc0be01f827e7289cb4e
shprexen <shprexenplus@gmail.com>
2021-05-13 22:47:26 +0300

    Added pages

--- LAB3/LAB3/urls.py
+++ LAB3/LAB3/urls.py
@@ -4,7 +4,7 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', include('pages.urls')),
     path('users/', include('users.urls')),
     path('users/',include('django.contrib.auth.urls'))
 ]
