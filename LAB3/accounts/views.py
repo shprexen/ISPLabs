70303ca1b8b18e4f0ceca81d123b928c741c3b49
shprexen <shprexenplus@gmail.com>
2021-06-04 16:54:42 +0300

    views code fixed

--- LAB3/accounts/views.py
+++ LAB3/accounts/views.py
@@ -8,56 +8,9 @@ from django.contrib.auth import authenticate, login, logout
 from .models import *
 from .forms import StudentForm, CreateUserForm
 from .decorators import unauthenticated_user
+from .schedule import *
 
 import requests
-
-
-class Lesson:
-    def __init__(self, lessonTime="-", subject="-", lessonType="-", employee="-", audience="-"):
-        self.lessonTime = lessonTime
-        self.subject = subject
-        self.lessonType = lessonType
-        self.employee = employee
-        self.audience = audience
-
-
-class Exam:
-    def __init__(self, lessonTime="-", subject="-", lessonType="-", employee="-", audience="-", datetime="-"):
-        self.lessonTime = lessonTime
-        self.subject = subject
-        self.lessonType = lessonType
-        self.employee = employee
-        self.audience = audience
-        self.datetime = datetime
-
-
-def get_schedule(schedule):
-    if schedule:
-        lessons = []
-        if not 'schedule' in schedule[0].keys():
-            for i in range(len(schedule)):
-                if len(schedule[i]['employee']) != 0:
-                    lesson = Lesson(schedule[i]['lessonTime'],
-                                    schedule[i]['subject'],
-                                    schedule[i]['lessonType'],
-                                    schedule[i]['employee'][0]['fio'],
-                                    schedule[i]['auditory'][0])
-                else:
-                    lesson = Lesson(schedule[i]['lessonTime'],
-                                    schedule[i]['subject'],
-                                    schedule[i]['lessonType'], )
-                lessons.append(lesson)
-        else:
-            for i in range(len(schedule)):
-                exam = Exam(schedule[i]['schedule'][0]['lessonTime'],
-                              schedule[i]['schedule'][0]['subject'],
-                              schedule[i]['schedule'][0]['lessonType'],
-                              schedule[i]['schedule'][0]['employee'][0]['fio'],
-                              schedule[i]['schedule'][0]['auditory'],
-                              schedule[i]['weekDay'])
-                lessons.append(exam)
-
-        return lessons
 
 
 @unauthenticated_user
